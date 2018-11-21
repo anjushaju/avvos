f5f32c831fcf8b39dba426cc4c72b994e31b39a7
anjushaju <anjushaju333@gmail.com>
2018-11-21 17:08:26 +0530

    setting up

--- avvos/avvos/items.py
+++ avvos/avvos/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class AvvosItem(scrapy.Item):
+class avvosItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     name = scrapy.Field()
