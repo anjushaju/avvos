62863f65ea4849414f3ca2e6c17f6759e549c803
anjushaju <anjushaju333@gmail.com>
2018-11-22 14:20:14 +0530

    settings

--- avvos/avvos/settings.py
+++ avvos/avvos/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3   
+DOWNLOAD_DELAY = 3   
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -44,7 +44,6 @@ DEFAULT_REQUEST_HEADERS = {
    'accept-encoding':'gzip, deflate, br',
    'accept-language':'en-GB,en-US;q=0.8,en;q=0.6',
    'cache-control':'no-cache','pragma':'no-cache',
-   'referer':'https://www.avvo.com/find-a-lawyer/all-lawyers/ny/all-cities-counties',
    'upgrade-insecure-requests':'1',
    'user-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'
 }
